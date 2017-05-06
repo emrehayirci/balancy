753c38f5bae0b6394fbdc97e759a7a7f8d04f55a
Emre Hayırcı <emrehayirci@gmail.com>
2017-05-06 16:02:14 +0300

    team dividing completed

--- index.android.js
+++ index.android.js
@@ -16,6 +16,7 @@ import {
   KeyboardAvoidingView,
   Button,
   FlatList,
+  TouchableHighlight,
 } from 'react-native';
 
 const dificultyLevels = [
@@ -32,27 +33,67 @@ export default class balancy extends Component {
         this.state={
             form_nameField: "",
             form_levelField: 3,
-            team1:[{name:123, level:1},{name:123, level:1},{name:123, level:1},{name:123, level:1},{name:123, level:1}],
+            all:[{name:'a', level:1},{name:'b', level:2},{name:'c', level:3},{name:'d', level:4},{name:'f', level:5}],
+            team1:[],
             team2:[{name:123, level:1},{name:123, level:1},{name:123, level:1},{name:123, level:1}],
         }
     }
-
     teamMemberItem(entry){
         return(
-            <View>
+            <View style={styles.teamItemStyle}>
                 <Text>{entry.index + 1}</Text>
                 <Text>{entry.item.name}</Text>                
             </View>
         )
-            
-
     }
-
+    addNewItem(){
+        console.log(this.state)
+        var item = {};
+        if(!this.state.form_nameField){
+            alert("İsim girmeyi unuttunuz!")
+            return;
+        }
+        item.name = this.state.form_nameField;
+        item.level = this.state.form_levelField;
+        var _all = this.state.all;
+        _all.push(item);
+        _all = _all.sort((a,b)=>{return(b.level-a.level)});//ascending order
+        this.setState({all:_all});
+        this.calculateTeams(_all);
+    }
+    calculateTeams(all){
+        team1 = [];
+        team2 = [];
+        team1sum = 0;
+        team2sum = 0;
+        all.forEach((item) => {
+            console.log(item);
+            if(team1.length < team2.length){
+                team1.push(item);
+                team1sum += item.level;
+            }
+            else if(team1.length > team2.length){
+                team2.push(item);
+                team2sum += item.level;
+            }
+            else{
+                if(team1sum > team2sum){
+                    team2.push(item);
+                    team2sum += item.level;
+                }
+                else{
+                    team1.push(item);
+                    team1sum += item.level;
+                }
+            }
+        })
+        this.setState({team1:team1})
+        this.setState({team2:team2})
+    }
     render() {
         return (
             <KeyboardAvoidingView>
                 <ScrollView bounces={true} >
-
 
                     <View  style={styles.addNewItemContainer}>
                         <TextInput
@@ -67,7 +108,11 @@ export default class balancy extends Component {
                                 )
                             })}
                         </Picker>
-                        <Button style = {styles.addButton} title=" + " color="#841584"  />
+                        <TouchableHighlight onPress={this.addNewItem.bind(this)}>
+                            <View style = {styles.addButton} >
+                                <Text>+</Text>
+                            </View>
+                        </TouchableHighlight>
                     </View>
 
                     <Text>Oluşturulan Takımlar</Text>
@@ -83,16 +128,14 @@ export default class balancy extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
   addNewItemContainer:{
     flexDirection:'row',
-    padding:5,
     flex:1,
+    padding:5,
+    margin:5,
+    backgroundColor:'#C8E6C9',
+    borderRadius:7,
+
   },
   teamsContainer:{
       flex:1,
@@ -101,9 +144,13 @@ const styles = StyleSheet.create({
   teamStyle:{
     flex:1,
   },
+  teamItemStyle:{
+      flexDirection:'row',
+  },
   addButton:{
     flex:0.5,
-    padding: 10
+    backgroundColor: '#388E3C',
+    padding: 10,
   },
   levelField:{
     flex:1,
